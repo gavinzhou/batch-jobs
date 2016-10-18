97b37281facd64bafde66d413763a49daf366c90
gavinzhou <gavin.zhou@gmail.com>
2016-10-18 07:40:27 +0000

    add logger

--- adduser.py
+++ adduser.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import jwt
-import uuid
+# -*- coding: utf-8 -*-
 from requests import post
 import os
+import logging
 
 
 key = os.environ["kongKey"]
@@ -11,70 +11,92 @@ kongadminapi = os.environ["kongAdminAPI"]
 username = os.environ["UserName"]
 writepassword = os.environ["WritePassword"]
 
+logging.getLogger("requests").setLevel(logging.WARNING)
 
-def add_apis(username, kongadminapi):
-    name = username + "-influxdb"
-    upstream_url = "http://" + name + ".default"
-    request_host = username + ".i.orangesys.io"
-    posturl = kongadminapi + "/apis"
-    data = {
-        'name': name,
-        'upstream_url': upstream_url,
-        'request_host': request_host
-    }
-    return True if post(posturl, data=data).status_code == '201' else False
+class CreateApis:
+    def __init__(self, username, kongadminapi, writepassword):
+        self.username = username
+        self.kongadminapi = kongadminapi
+        self.writepassword = writepassword
 
 
-def enable_jwt(username, kongadminapi):
-    posturl = kongadminapi + "/apis/" + username + "-influxdb/plugins"
-    data = {'name': 'jwt'}
-    return post(posturl, data=data)
+    def add_apis(self):
+        name = self.username + "-influxdb"
+        upstream_url = "http://" + name + ".default"
+        request_host = self.username + ".i.orangesys.io"
+        posturl = self.kongadminapi + "/apis"
+        data = {
+            'name': name,
+            'upstream_url': upstream_url,
+            'request_host': request_host
+        }
+        return True if post(posturl, data=data).status_code == 201 else False
+    
+    
+    def enable_jwt(self):
+        posturl = self.kongadminapi + "/apis/" + self.username + "-influxdb/plugins"
+        data = {'name': 'jwt'}
+        return True if post(posturl, data=data).status_code == 201 else False
+    
+    
+    def enable_correlation_id(self):
+        posturl = self.kongadminapi + "/apis/" + self.username + "-influxdb/plugins"
+        data = {
+            'name': 'correlation-id',
+            'config.header_name': 'Orangesys-Request-ID',
+            'config.generator': "uuid#counter",
+            'config.echo_downstream': 'false'
+        }
+        return True if post(posturl, data=data).status_code == 201 else False
+    
+    
+    def request_transformer(self):
+        posturl = self.kongadminapi + "/apis/" + self.username + "-influxdb/plugins"
+        querystring = "u:_write,p:" + self.writepassword
+        data = {
+            'name': 'request-transformer',
+            'config.remove.querystring': 'jwt',
+            'config.add.querystring': querystring
+        }
+        return True if post(posturl, data=data).status_code == 201 else False
 
 
-def enable_correlation_id(username, kongadminapi):
-    posturl = kongadminapi + "/apis/" + username + "-influxdb/plugins"
-    data = {
-        'name': 'correlation-id',
-        'config.header_name': 'Orangesys-Request-ID',
-        'config.generator': "uuid#counter",
-        'config.echo_downstream': 'false'
-    }
-    return post(posturl, data=data)
+class CreateJwtToken:
+    def __init__(self, username, kongadminapi, key, secret):
+        self.username = username
+        self.kongadminapi = kongadminapi
+        self.key = key
+        self.secret = secret
 
 
-def request_transformer(username, kongadminapi, writepassword):
-    posturl = kongadminapi + "/apis/" + username + "-influxdb/plugins"
-    querystring = "u:_write,p:" + writepassword
-    data = {
-        'name': 'request-transformer',
-        'config.remove.querystring': 'jwt',
-        'config.add.querystring': querystring
-    }
-    return post(posturl, data=data)
-
-
-def add_consumer(username, kongadminapi):
-    posturl = kongadminapi + "/consumers"
-    data = { 'username': username}
-    return True if post(posturl, data=data).status_code == '201' else False
-
-
-def add_jwt(username, kongadminapi, key, secret):
-    posturl = kongadminapi + "/consumers/" + username + "/jwt"
-    data = {
-        'key': key,
-        'secret': secret
-    }
-    return post(posturl, data=data)
+    def add_consumer(self):
+        posturl = self.kongadminapi + "/consumers"
+        data = { 'username': self.username}
+        return True if post(posturl, data=data).status_code == 201 else False
+    
+    
+    def add_jwt(self):
+        posturl = self.kongadminapi + "/consumers/" + self.username + "/jwt"
+        data = {
+            'key': self.key,
+            'secret': self.secret
+        }
+        return True if post(posturl, data=data).status_code == 201 else False
 
 
 def main():
-    if add_apis(username, kongadminapi):
-        enable_jwt(username, kongadminapi)
-        enable_correlation_id(username, kongadminapi)
-        request_transformer(username, kongadminapi, writepassword)
-    if add_consumer(username, kongadminapi):
-        add_jwt(username, kongadminapi, key, secret)
+    FORMAT = '%(asctime)s %(levelname)s %(message)s'
+    logging.basicConfig(format = FORMAT, level=logging.INFO)
+    c = CreateApis(username, kongadminapi, writepassword)
+    if c.add_apis():
+        c.enable_jwt()
+        c.enable_correlation_id()
+        c.request_transformer()
+        logging.info("create %s api to kong", username)
+    j = CreateJwtToken(username, kongadminapi, key, secret)
+    if j.add_consumer():
+        j.add_jwt()
+        logging.info("create %s jwt token to kong", username)
 
 
 if __name__ == '__main__':
